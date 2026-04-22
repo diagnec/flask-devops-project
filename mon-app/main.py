d9a21925afff0d1721762da176ad757ddb898ba1
diagnec <diagnec809@gmail.com>
2026-04-22 18:49:45 +0000

    Mise à jour

--- mon-app/main.py
+++ mon-app/main.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify  # Ajout de jsonify
+import os
+import psycopg2
 
 app = Flask(__name__)
 
+# Connexion à la base de données
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+        return conn
+    except Exception as e:
+        return None
+
 @app.route("/")
 def home():
-    return "Hello Cheikh,Bienvenue dans Kubernetes"
+    return "Hello Cheikh, Bienvenue dans Kubernetes"
 
 @app.route("/health")
 def health():
     return {"Status": "ok"}
 
+@app.route("/db")
+def check_db():
+    conn = get_db_connection()
+    if conn:
+        conn.close()
+        return jsonify({"database": "connected"})
+    else:
+        return jsonify({"database": "not connected"}), 500
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
